8f62fa3be0038f6dbba54b6eb3fa6e57f51f7595
Sano <36809574+Sanokei@users.noreply.github.com>
2022-10-03 23:45:16 -0400

    device size block and home page linkage to works
    Added a block for devices too small to look at the website and also added a way to link the links under the home page to go to the specific blurbs on the second page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -50,6 +50,107 @@ $( document ).ready(function() {
     }
 
   });
+  
+  $('.next-tele').click(function(){
+    var curActive = $('.side-nav').find('.is-active'),
+    curPos = $('.side-nav').children().index(curActive),
+    lastItem = $('.side-nav').children().length - 4,
+    nextPos = lastItem;
+
+    updateNavs(lastItem);
+    updateContent(curPos, nextPos, lastItem);
+    var $this = $(this),
+          curLeft = $('.slider').find('.slider--item-left'),
+          curLeftPos = $('.slider').children().index(curLeft),
+          curCenter = $('.slider').find('.slider--item-center'),
+          curCenterPos = $('.slider').children().index(curCenter),
+          curRight = $('.slider').find('.slider--item-right'),
+          curRightPos = $('.slider').children().index(curRight),
+          totalWorks = $('.slider').children().length,
+          $left = $('.slider--item-left'),
+          $center = $('.slider--item-center'),
+          $right = $('.slider--item-right'),
+          $item = $('.slider--item');
+    if (curLeftPos < totalWorks - 1 && curCenterPos < totalWorks - 1 && curRightPos < totalWorks - 1) {
+      $left.removeClass('slider--item-left').next().addClass('slider--item-left');
+      $center.removeClass('slider--item-center').next().addClass('slider--item-center');
+      $right.removeClass('slider--item-right').next().addClass('slider--item-right');
+    }
+    else {
+      if (curLeftPos === totalWorks - 1) {
+        $item.removeClass('slider--item-left').first().addClass('slider--item-left');
+        $center.removeClass('slider--item-center').next().addClass('slider--item-center');
+        $right.removeClass('slider--item-right').next().addClass('slider--item-right');
+      }
+      else if (curCenterPos === totalWorks - 1) {
+        $left.removeClass('slider--item-left').next().addClass('slider--item-left');
+        $item.removeClass('slider--item-center').first().addClass('slider--item-center');
+        $right.removeClass('slider--item-right').next().addClass('slider--item-right');
+      }
+      else {
+        $left.removeClass('slider--item-left').next().addClass('slider--item-left');
+        $center.removeClass('slider--item-center').next().addClass('slider--item-center');
+        $item.removeClass('slider--item-right').first().addClass('slider--item-right');
+      }
+    }
+  });
+  
+  $('.prev-tele').click(function(){
+    var curActive = $('.side-nav').find('.is-active'),
+    curPos = $('.side-nav').children().index(curActive),
+    lastItem = $('.side-nav').children().length - 4,
+    nextPos = lastItem;
+
+    updateNavs(lastItem);
+    updateContent(curPos, nextPos, lastItem);
+
+    var $this = $(this),
+          curLeft = $('.slider').find('.slider--item-left'),
+          curLeftPos = $('.slider').children().index(curLeft),
+          curCenter = $('.slider').find('.slider--item-center'),
+          curCenterPos = $('.slider').children().index(curCenter),
+          curRight = $('.slider').find('.slider--item-right'),
+          curRightPos = $('.slider').children().index(curRight),
+          totalWorks = $('.slider').children().length,
+          $left = $('.slider--item-left'),
+          $center = $('.slider--item-center'),
+          $right = $('.slider--item-right'),
+          $item = $('.slider--item');
+    if (curLeftPos !== 0 && curCenterPos !== 0 && curRightPos !== 0) {
+      $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
+      $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
+      $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
+    }
+    else {
+      if (curLeftPos === 0) {
+        $item.removeClass('slider--item-left').last().addClass('slider--item-left');
+        $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
+        $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
+      }
+      else if (curCenterPos === 0) {
+        $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
+        $item.removeClass('slider--item-center').last().addClass('slider--item-center');
+        $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
+      }
+      else {
+        $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
+        $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
+        $item.removeClass('slider--item-right').last().addClass('slider--item-right');
+      }
+    }
+  });
+
+  $('.tele').click(function(){
+
+    var curActive = $('.side-nav').find('.is-active'),
+        curPos = $('.side-nav').children().index(curActive),
+        lastItem = $('.side-nav').children().length - 4,
+        nextPos = lastItem;
+
+    updateNavs(lastItem);
+    updateContent(curPos, nextPos, lastItem);
+
+  });
 
   $('.cta').click(function(){
 
@@ -176,7 +277,7 @@ $( document ).ready(function() {
     });
 
   }
-
+  
   function workSlider() {
 
     $('.slider--prev, .slider--next').click(function() {
