724478952d8dd67254c7e542f08914d7e8d7ce29
Sano <36809574+Sanokei@users.noreply.github.com>
2022-12-18 23:34:48 -0500

    Address bar fix

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -544,20 +544,6 @@ var randomDic = locations.random();
     });
 
   }
-  function activateFullscreen(element) {
-    if(element.requestFullscreen) {
-      element.requestFullscreen();        // W3C spec
-    }
-    else if (element.mozRequestFullScreen) {
-      element.mozRequestFullScreen();     // Firefox
-    }
-    else if (element.webkitRequestFullscreen) {
-      element.webkitRequestFullscreen();  // Safari
-    }
-    else if(element.msRequestFullscreen) {
-      element.msRequestFullscreen();      // IE/Edge
-    }
-  };
   window.addEventListener("load",function() {
     // Set a timeout...
     setTimeout(function(){
